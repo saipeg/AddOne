2fc01b7af5bb2a1ce6d2a2f1cb9f485e200b7727
Andry Laptop <saipeg@yandex.ru>
2020-05-16 19:17:54 +0300

    init commit

---
+++ src/AddOne.java
@@ -0,0 +1,10 @@
+// Autor Andrey S
+
+// data 16.05.2020
+
+public class AddOne {
+    public static void main(String[] args) {
+        int i = 2;
+        System.out.print(++i + ++i + i++ + i);
+    }
+}
