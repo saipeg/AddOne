fa7b5ffe6810f58242824b40286839d1d4dede4c
Andrey <saipeg@yandex.ru>
2020-05-18 01:29:38 +0300

    add new block

--- src/main/java/com/andrey/One.java
+++ src/main/java/com/andrey/One.java
@@ -9,6 +9,11 @@ package com.andrey;
 public class One {
     public static void main(String[] args) {
         int i = 2;
-        System.out.print(++i + ++i + i++ + i);
+        System.out.println(++i + ++i + i++ + i);
+
+        while (i < 10) {
+            System.out.println("i = " + i);
+            i++;
+        }
     }
 }
